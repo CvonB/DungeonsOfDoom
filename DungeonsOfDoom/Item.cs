29f129086273626cce5aaa176d65693bb22f0314
Martin Holmberg <martin.holmberg@brightstraining.com>
2022-11-08 21:35:19 +0100

    La till SubClasses för Weapon: Axe, Sword, Spear och gav dem förutom namn Power också. - Martin

--- DungeonsOfDoom/Item.cs
+++ DungeonsOfDoom/Item.cs
@@ -19,12 +19,44 @@ namespace DungeonsOfDoom
         // Vi kan specifiera det i SubClassen annars - Christian
 
         //public bool Consumable => Type == "Consumable";
-        public string Type  => this.GetType().ToString().Split('.')[1]; 
+        public string Type => this.GetType().ToString().Split('.')[1];
     }
 
+   //La till SubClasses för Weapon: Axe, Sword, Spear och gav dem förutom namn Power också. - Martin
     class Weapon : Item
     {
         public Weapon() : base("Axe")
+        {
+
+        }
+
+        public Weapon(string weaponType, int power) : base(weaponType)
+        {
+            Power = power;
+        }
+
+        public int Power { get; set; }
+    }
+
+    class Sword : Weapon
+    {
+        public Sword() : base("Sword", 20)
+        {
+
+        }
+    }
+
+    class Axe : Weapon
+    {
+        public Axe() : base("Axe", 15)
+        {
+
+        }
+    }
+
+    class Spear : Weapon
+    {
+        public Spear() : base("Spear", 10)
         {
 
         }
@@ -40,13 +72,13 @@ namespace DungeonsOfDoom
 
 
     //TODO: Konvertera denna enum till en SubClass istället
-    enum Items 
-        {
-            Sword = 0,
-            Axe = 1,
-            Spear = 2,
-            Pear = 3,
-            Apple = 4,
-        }
+    enum Items
+    {
+        Sword = 0,
+        Axe = 1,
+        Spear = 2,
+        Pear = 3,
+        Apple = 4,
+    }
 
 }
